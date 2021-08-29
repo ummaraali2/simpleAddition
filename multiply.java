4a8c65336ccf7a7bd514ada40ec4c77cd40240e9
Ummara <ummaraali2020@gmail.com>
2021-08-30 00:16:39 +0530

    added multiply.java

--- multiply.java
+++ multiply.java
@@ -1,8 +1,8 @@
-public class diff{
+public class multiply{
     public static void main(string[] args)
     {
       int a = 20;
       int b=12;
-     system.out.println("the sum is"+a-b);
+     system.out.println("the product is"+a*b);
     }
 }
